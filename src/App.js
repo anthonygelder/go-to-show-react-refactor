d4b4d52909b6055deb0a476c8b59ff8e39337061
Anthony Gelder <anthony.gelder@gmail.com>
2020-03-10 22:18:53 -0700

    displaying data in list

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import Map from './Map/Map'
-import Search from './Search/Search'
 import Results from './Results/Results'
 import './App.css'
 
@@ -9,10 +8,61 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
+      city: '',
+      date: '',
+      results: [],
+      lat: 0,
+      lng: 0
     }
   }
 
-  
+  updateCity(city) {
+    this.setState({city: city})
+  }
+
+  updateDate(date) {
+    this.setState({date: date})
+  }
+
+  getEvents(id, date) {
+    const apiUrl = `https://api.songkick.com/api/3.0/metro_areas/${id}/calendar.json?apikey=lKGlBIRmnawI3yka&min_date=${date}&max_date=${date}`;
+    fetch(apiUrl)
+    .then(response => response.json())
+    .then(responseJson => {
+        if (responseJson.status === 'error') {
+            throw new Error(responseJson.message)
+        }
+        this.setState({results: responseJson.resultsPage.results.event})
+    })
+    .catch(error => {
+        console.log(error)
+    })
+}
+
+getCityId(city, date) {
+    const apiUrl = `https://api.songkick.com/api/3.0/search/locations.json?query=${city}&apikey=lKGlBIRmnawI3yka`;
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(responseJson => {
+            if (responseJson.status === 'error') {
+                throw new Error(responseJson.message)
+            }
+            // console.log(responseJson)
+            this.setState({ lat: responseJson.resultsPage.results.location[0].city.lat,
+                            lng: responseJson.resultsPage.results.location[0].city.lng})
+            this.getEvents(responseJson.resultsPage.results.location[0].metroArea.id, date)
+        })
+        .catch(error => {
+            console.log(error)
+        });
+}
+
+handleSubmit(e) {
+  e.preventDefault()
+  let city = this.state.city
+  let date = this.state.date
+  this.getCityId(city, date)
+}
 
   render() {
     return (
@@ -21,10 +71,18 @@ class App extends Component {
           <h1>
             Go To Show
           </h1>
-          <Search />
+          <form onSubmit={e => this.handleSubmit(e)}>
+                        <label htmlFor="city">City:</label>
+                        <input required type="text" name="city" id="city-search" onChange={e => this.updateCity(e.target.value)}/>
+                        <label htmlFor="date">Date:</label>
+                        <input type="date" name="date" id="date-search"  onChange={e => this.updateDate(e.target.value)}/>
+                    <button type="submit">
+                        Search
+                    </button>
+                </form>
         </header>
         <main className='App'>
-          <Results />
+          <Results shows={this.state.results} />
           <Map />
         </main>
       </div>
--- src/Results/Results.js
+++ src/Results/Results.js
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react'
 import './Results.css'
 
-function Results(props) {
-    return (
-        <aside>
-            <h2>Results</h2>
-        </aside>
-    )
+class Results extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            results: []
+        }
+    }
+
+
+
+    generateHTML(shows) {
+        console.log('hello')
+        console.log(shows)
+    }
+
+
+
+
+    render() {
+        console.log(this.props)
+        let results;
+
+        if (this.props.shows.length > 0) {
+            results = this.generateHTML(this.props.shows)
+        } else {
+            results = '';
+        }
+
+        
+        return (
+            <>
+                <div id="tableContainer">
+                    <table id="results">
+                        {results}
+                    </table>
+                </div> 
+            </>
+        )
+    }
 }
 
 export default Results;
