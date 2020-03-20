308abf3eb1e78eea61903cc7e7f643077143bf57
Anthony Gelder <anthony.gelder@gmail.com>
2020-03-19 20:48:50 -0700

    adding google maps api

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react'
+import GoogleMapReact from 'google-map-react'
 import './Map.css'
 
-function Map(props) {
-    return (
-        <>
-            <h1>Map</h1>
-        </>
-    )
+
+class Map extends Component {
+    static defaultProps = {
+        center: {
+          lat: 59.95,
+          lng: 30.33
+        },
+        zoom: 11
+      };
+    
+    render() {
+        return (
+        // Important! Always set the container height explicitly
+        <div style={{ height: '100vh', width: '100%' }}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyCiv03ah7ZjCY_Nx6S6ZFnrX32ThZX6_-w' }}
+                defaultCenter={this.props.center}
+                defaultZoom={this.props.zoom}
+                >
+                {/* <AnyReactComponent
+                    lat={59.955413}
+                    lng={30.337844}
+                    text="My Marker"
+                /> */}
+            </GoogleMapReact>
+        </div>
+        )
+    }
 }
 
 export default Map;
--- src/Results/Results.js
+++ src/Results/Results.js
@@ -16,22 +16,51 @@ class Results extends Component {
         console.log(shows)
     }
 
+    urlStr(str) {
+        return str.replace(/ /g,"+");
+    }
 
+    convertTime(time) {
+        if (time === null) {
+            return 'N/A'
+        } else {
+            let amPm;
+            let militaryHrMn = time.substring(0, time.length - 3);
+            let minutes = militaryHrMn.substring(3);
+            let hour = time.substring(0, time.length - 6);
+            
+            if (hour > 12) {
+                hour = hour - 12;
+                amPm = "pm";
+            } else {
+                amPm = "am";
+            }
+            return `${hour}:${minutes}${amPm}`;
+        }
+    }
 
 
     render() {
         console.log(this.props)
-        let results;
+        // let results;
 
-        if (this.props.shows.length > 0) {
-            results = this.generateHTML(this.props.shows)
-        } else {
-            results = '';
-        }
+        // if (this.props.shows.length > 0) {
+        //     results = this.generateHTML(this.props.shows)
+        // } else {
+        //     results = '';
+        // }
 
         const concerts = this.props.shows.map(show => {
+            const city = show.location.city;
+            const mapsUrl = 'https://www.google.com/maps/search/?api=1&query=' + this.urlStr(show.venue.displayName)
+            
             console.log(show)
-            return (<p key={show.id}>{show.id}</p>)
+            return (<tr key={show.id}>
+                        <td><a href={show.uri} rel="noopener noreferrer" target="_blank">{show.performance[0].displayName}</a></td>
+                        <td><a href={mapsUrl} rel="noopener noreferrer" target="_blank">{show.venue.displayName}</a></td>
+                        <td valign="top">{this.convertTime(show.start.time)}</td>
+                        <td>{city.substring(0, city.length - 8)}</td>
+                    </tr>)
             }
         )
         
@@ -49,10 +78,9 @@ class Results extends Component {
                         </tr>
                     </tbody>
                     <tbody>
-                        
+                        {concerts}
                     </tbody>
                 </table>
-                {concerts}
             </div> 
         )
     }
